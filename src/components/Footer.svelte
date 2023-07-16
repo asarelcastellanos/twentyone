e375c709323f7b20ffbfc9da0194e47110d189b7
Asarel Castellanos <asarelc@gmail.com>
2023-07-16 13:46:36 -0700

    social links are responsive

---
+++ src/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script>
+  import SocialLinks from "./SocialLinks.svelte";
+
+</script>
+<footer class="bg-slate-100 flex flex-col p-4 items-center">
+    <SocialLinks />
+    <p>Designed & Built by Asarel Castellanos</p>
+</footer>
---
+++ src/components/SocialLinks.svelte
@@ -0,0 +1,34 @@
+<script>
+  const SocialLinks = [
+    {
+      name: "Github",
+      link: "https://github.com/asarelcastellanos",
+      icon: '../../socialIcons/github.svg'
+    },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/asarelcastellanos/",
+      icon: '../../socialIcons/linkedin.svg'
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/asarelathome",
+      icon: '../../socialIcons/twitter.svg'
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/asarelcastellanos/",
+      icon: '../../socialIcons/instagram.svg'
+    },
+  ];
+</script>
+
+<ul class="flex md:flex-col md:absolute md:bottom-8 md:left-8">
+  {#each SocialLinks as SocialLink}
+    <li class="h-8 w-8 mr-4 md:mb-4">
+      <a href={SocialLink.link} target="_blank">
+        <img src={SocialLink.icon} alt={SocialLink.name}/>
+      </a>
+    </li>
+  {/each}
+</ul>
